28479f57b1bb7df4ff228f6df90bac74faf3be5a
abdulhafizr <abdulhafizramadan.17@gmail.com>
2020-12-24 21:31:34 +0700

    register page v3

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import register_bg from './images/register-bg.jpg';
+import login_bg from './images/login-bg.jpg';
+
+export {register_bg, login_bg};
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,56 +1,58 @@
-import { Button, Grid, Card, TextField, FormControl, makeStyles, Icon } from '@material-ui/core';
-import React, {Fragment} from 'react';
+import {Grid, Card, FormControl, TextField, Button, Icon, makeStyles} from '@material-ui/core';
+import React from 'react';
+import {register_bg} from '../../assets';
 
 const useStyles = makeStyles((theme) => ({
-    '& > *': {
-        margin: theme.spacing(1)
+    root: {
+        height: '100vh',
+        spacing: 0,
+        background: `url(${register_bg})`,
+        backgroundRepeat: 'no-repeat',
+        backdropFilter: 'blur(1px)',
+        backgroundSize: 'cover',
+        overFlow: 'hidden'
     },
     form_login: {
-        padding: 12,
-        minWidth: 345
+        background: '#fff',
+        padding: 10,
     },
     mb: {
-        marginBottom: 5
+        marginBottom: 4
+    },
+    mr: {
+        marginRight: 15
     }
 }))
 
 const Register = () => {
     const classes = useStyles();
     return (
-        <Fragment>
-            <Grid container 
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justify="center"
-                    style={{ minHeight: '100vh' }}
-                >
-                <Grid item lg={5}>
-                    <Card>
-                        <div className={classes.form_login}>
-                            <h2 style={
-                                {textAlign: 'center', fontSize: 26}
-                            }>
-                                Registrasi
-                            </h2>
-                            <form className={classes.root} autoComplete="off">
-                                <FormControl fullWidth className={classes.mb}>
-                                    <TextField type="email" id="standard-basic" required={true} label="Email" />
-                                </FormControl>
-                                <FormControl fullWidth className={classes.mb}>
-                                    <TextField type="password" id="standard-basic" required={true} label="Password" />
-                                </FormControl>
-                                <FormControl fullWidth className={classes.mb}>
-                                    <TextField type="password" id="standard-basic" required={true} label="Konfirmasi Password" />
-                                </FormControl>
-                                <Button variant="contained" size="small" style={{marginRight: 4}} color="secondary">Kembali</Button>
-                                <Button variant="contained" size="small" endIcon={<Icon>send</Icon>} color="primary">Registrasi</Button>
-                            </form>
-                        </div>
-                    </Card>
-                </Grid>
+        <Grid container justify="flex-end" alignItems="center" className={classes.root}>
+            <Grid item sm={4}>
+                <Card className={classes.mr}>
+                    <div className={classes.form_login}>
+                        <h2 style={
+                            {textAlign: 'center', fontSize: 26}
+                        }>
+                            Registrasi
+                        </h2>
+                        <form autoComplete="off">
+                            <FormControl fullWidth className={classes.mb}>
+                                <TextField type="email" id="standard-basic" required={true} label="Email" />
+                            </FormControl>
+                            <FormControl fullWidth className={classes.mb}>
+                                <TextField type="password" id="standard-basic" required={true} label="Password" />
+                            </FormControl>
+                            <FormControl fullWidth className={classes.mb}>
+                                <TextField type="password" id="standard-basic" required={true} label="Konfirmasi Password" />
+                            </FormControl>
+                            <Button variant="contained" size="small" style={{marginRight: 4}} color="secondary">Kembali</Button>
+                            <Button variant="contained" size="small" endIcon={<Icon>send</Icon>} color="primary">Registrasi</Button>
+                        </form>
+                    </div>
+                </Card>
             </Grid>
-       </Fragment>
+        </Grid>
     )
 }
 
