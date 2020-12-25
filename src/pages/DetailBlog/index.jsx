b21378e569091ef7ab8927dcf8684528ffb1aef9
abdulhafizr <abdulhafizramadan.17@gmail.com>
2020-12-25 21:42:10 +0700

    make detail blog page

--- src/pages/DetailBlog/index.jsx
+++ src/pages/DetailBlog/index.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {Card, CardContent, Container, Grid, Typography} from '@material-ui/core';
+import useStyles from './useStyles';
+import {blog_image} from '../../assets';
 
 const DetailBlog = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Detail Blog</h1>
-        </div>
+        <Container className={classes.root}>
+            <Grid container spacing={4}>
+                <Grid item sm={12} md={9} style={{marginBottom: 8}}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, fugiat.
+                            </Typography>
+                            <img src={blog_image} className={classes.image_blog} alt="img_post"/>
+                            <Typography variant="caption" color="primary">
+                                author : <span className={classes.author}>abdul hafiz ramadan</span>
+                            </Typography>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sm={12} md={3} style={{marginBottom: 8}}>
+                    <Card>
+                        <CardContent>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi debitis, ipsum accusantium asperiores consequatur quam nesciunt reiciendis dignissimos voluptas sit magnam optio tenetur officiis fugiat, animi pariatur nam ea voluptatum!</p>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
---
+++ src/pages/DetailBlog/useStyles.js
@@ -0,0 +1,16 @@
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: 10
+    },
+    image_blog: {
+        height: 340,
+        display: 'block'
+    },
+    author: {
+        color: '#111'
+    }
+}))
+
+export default useStyles;
